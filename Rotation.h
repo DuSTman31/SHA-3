9ce9eff85094049074f87298c46babf95274ed62
DuSTman31 <d.horne@danielhorne.co.uk>
2017-06-23 08:45:25 +0100

    Potential fix for issue #3, raised by coooooooooorey.

--- Rotation.h
+++ Rotation.h
@@ -5,14 +5,23 @@
 
 // As we're not using assembly, we can't use the native rotation instructions
 //  replace it with a small inline
-static inline uint64_t rotateLeft(uint64_t x, int n)
+static inline uint64_t rotateLeft(uint64_t x, unsigned int n)
 {
-	return ((x << n) | (x >> (64 - n)));
+	const unsigned int mask = (8*sizeof(x) - 1);  // assumes width is a power of 2.
+
+	// assert ( (c<=mask) &&"rotate by type width or more");
+	n &= mask;
+	return (x << n) | (x >> ((-n)&mask));
 }
 
 static inline uint64_t rotateRight(uint64_t x, int n)
 {
-	return ((x >> n) | (x << (64 - n)));
+	const unsigned int mask = (8 * sizeof(x) - 1);  // assumes width is a power of 2.
+
+	// assert ( (c<=mask) &&"rotate by type width or more");
+	n &= mask;
+	return (x >> n) | (x << ((-n)&mask));
+
 }
 
 #endif //ROTATION_H_
