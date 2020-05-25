5051e53b2b0ca6caeea870bc1cf2985f42ea39f3
DuSTman31 <d.horne@danielhorne.co.uk>
2020-05-26 00:20:22 +0100

    Move readme to base dir so that it shows up on Github. Moved static functions in keccak.cpp into anonymous namespace.

--- SHA-3/Keccak.cpp
+++ SHA-3/Keccak.cpp
@@ -6,25 +6,33 @@
 
 // Constants of the Keccak algorithm.
 
-constexpr uint64_t RC[] = {
-	0x0000000000000001L, 0x0000000000008082L, 0x800000000000808aL,
-	0x8000000080008000L, 0x000000000000808bL, 0x0000000080000001L,
-	0x8000000080008081L, 0x8000000000008009L, 0x000000000000008aL,
-	0x0000000000000088L, 0x0000000080008009L, 0x000000008000000aL,
-	0x000000008000808bL, 0x800000000000008bL, 0x8000000000008089L,
-	0x8000000000008003L, 0x8000000000008002L, 0x8000000000000080L,
-	0x000000000000800aL, 0x800000008000000aL, 0x8000000080008081L,
-	0x8000000000008080L, 0x0000000080000001L, 0x8000000080008008L
-};
+namespace {
+	constexpr uint64_t RC[] = {
+		0x0000000000000001L, 0x0000000000008082L, 0x800000000000808aL,
+		0x8000000080008000L, 0x000000000000808bL, 0x0000000080000001L,
+		0x8000000080008081L, 0x8000000000008009L, 0x000000000000008aL,
+		0x0000000000000088L, 0x0000000080008009L, 0x000000008000000aL,
+		0x000000008000808bL, 0x800000000000008bL, 0x8000000000008089L,
+		0x8000000000008003L, 0x8000000000008002L, 0x8000000000000080L,
+		0x000000000000800aL, 0x800000008000000aL, 0x8000000080008081L,
+		0x8000000000008080L, 0x0000000080000001L, 0x8000000080008008L
+	};
 
-constexpr int R[] = {
-	0, 1, 62, 28, 27, 36, 44, 6, 55, 20, 3, 10, 43,
-	25, 39, 41, 45, 15, 21, 8, 18, 2, 61, 56, 14
-};
+	constexpr int R[] = {
+		0, 1, 62, 28, 27, 36, 44, 6, 55, 20, 3, 10, 43,
+		25, 39, 41, 45, 15, 21, 8, 18, 2, 61, 56, 14
+	};
 
+	constexpr int index(int x)
+	{
+		return x < 0 ? index(x + 5) : x % 5;
+	}
 
-static inline constexpr int index(int x);
-static inline constexpr int index(int x, int y);
+	constexpr int index(int x, int y)
+	{
+		return index(x) + 5 * index(y);
+	}
+}
 
 // Function to create the state structure for keccak application, of size length
 //   (where length is the number of bits in the hash)
@@ -239,15 +247,7 @@ void KeccakBase::processBuffer()
 	bufferLen = 0;
 }
 
-static inline constexpr int index(int x)
-{
-	return x < 0 ? index(x + 5) : x % 5;
-}
 
-static inline constexpr int index(int x, int y)
-{
-	return index(x) + 5 * index(y);
-}
 
 struct keccakfState
 {
